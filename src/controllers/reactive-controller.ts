0b23b35bb3c83be344a7dcb2e1356e622da78c77
Valentin Degenne <vdegenne@gmail.com>
2025-07-14 16:13:58 +0200

    1.7.6

--- src/controllers/reactive-controller.ts
+++ src/controllers/reactive-controller.ts
@@ -11,7 +11,7 @@ import {
 	type ReactiveControllerHost,
 	type ReactiveElement,
 } from 'lit'
-import {type PropertyValues, ReactiveObject} from 'snar'
+import {type PropertyValues, ReactiveObject, type UpdateCalls} from 'snar'
 
 const hostedMethods = new Set([
 	'hostConnected',
